5add8f656890ad777239505f35ef32cbd6269af9
amamdouhmahfouz <abdelrahman.abdelghany99@eng-st.cu.edu.eg>
2021-05-29 17:00:33 +0200

    more types

--- rules/test2.c
+++ rules/test2.c
@@ -1,11 +1,24 @@
 int a = 2;
 int z = a;
+const int aa = 900;
+a++;
+char ch = 'w';
+
 /*
 multiline comment
 */
 int b = 3;
 // this is a comment
-
-int testFunc(int param1, int param2) {
+int func33(int aaaa, int ccc);
+int main(int param1, int param2) {
     int q = func1();
+    
+    switch(a) {
+        case 3: {
+
+        }
+    }
+
+    double d = 2.0;
+
 }
